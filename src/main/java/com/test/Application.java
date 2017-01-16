07890ed2b35656786765c9afbaead22a599c1680
Harshil Dhariwal <harshil.dhariwal@volkswagen.co.in>
2017-01-16 12:58:54 +0530

    setting up example as per spring boot docs

---
+++ src/main/java/com/test/Application.java
@@ -0,0 +1,27 @@
+package com.test;
+
+/**
+ * Created by W18NM36 on 1/16/2017.
+ */
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+
+}
---
+++ src/main/java/com/test/HelloController.java
@@ -0,0 +1,18 @@
+package com.test;
+
+/**
+ * Created by W18NM36 on 1/16/2017.
+ */
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
