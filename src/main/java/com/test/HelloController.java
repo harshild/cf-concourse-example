bfca9be38522bff4601aab79524de40398eda0cb
Harshil Dhariwal <harshil.dhariwal@volkswagen.co.in>
2017-01-18 11:50:58 +0530

    Message updated

--- src/main/java/com/test/HelloController.java
+++ src/main/java/com/test/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot Reloaded!";
+        return "Greetings from Spring Boot Once again!";
     }
 
 }
